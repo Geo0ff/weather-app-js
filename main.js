67eb28cd810ce994b4eaf651d8e6734c0ab018a8
Administrator <administrator@CN-2.lan>
2019-03-21 17:43:32 +0000

    changes

--- main.js
+++ main.js
@@ -1,17 +1,10 @@
+
+
 function startTime() {
     var date = new Date();
     var h = date.getHours();
     var m = date.getMinutes();
     var s = date.getSeconds();
-    var session = ""
-   
-
-
-
-    if (h >= 12) {
-        session = ""
-        
-    }
 
 
     h = (h < 10) ? "0" + h:h;
@@ -19,15 +12,67 @@ function startTime() {
     s = (s < 10) ? "0" + s:s;
 
 
-    let time = h + ":" + m + ":" + s + "" + session;
+    let time = h + ":" + m + ":" + s + "";
     
     document.getElementById('clockDisplay').innerText = time;
     document.getElementById('clockDisplay').innerContent = time;
 
     setTimeout(startTime, 1000);
-  }
+}
 
-    startTime();
+startTime();
+
+
+var getDate = new Date();
+// document.getElementById("date").innerHTML = getDate.getFullYear();
+// document.getElementById("date").innerHTML = getDate;
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+document.getElementById("date").innerHTML = months[getDate.getMonth()];
+
+
+
+
+
+// getDate();
+
+// var date = new Date();
+
+//     function startDate() {
+//         var d = date.getDate();
+//         var mo = date.getMonth();
+//         var y = date.getFullYear();
+    
+//         let date1 = d + "" + mo + "" + y;
+
+//         document.getElementById("Date").innerHTML = date;
+
+    
+//         }
+    
+//         startDate();
+    
+
+
+//     document.getElementById("Date").innerHTML = date;
+
+
+    // var d = new Date();
+
+    // function day () {
+    //     d.getDate();
+    // }
+
+    // function month() {
+    //     d.getMonth();
+    // }
+
+    // function year() {
+    //     d.getFullYear();
+    // }
+
+
+
+    
 
 
 const currentLocation = document.getElementsByClassName('sevenDayForecast')
